23bc9fbf83635b2bac509efe395af54c458682eb
Jose Castillo Lema <josecastillolema@gmail.com>
2026-07-08 11:51:04 +0200

    Enable UDP and reuse existing np template
    Signed-off-by: Jose Castillo Lema <josecastillolema@gmail.com>
    
        gitleaks.version: 8.30.0
        gitleaks.check-secrets: ENABLED

--- task.py
+++ task.py
@@ -908,39 +908,20 @@ class Task(ABC):
 
     def create_network_policy_ns_selector(self, port: int, allow_namespace: str) -> str:
         self.create_network_policy(port, NP_ACTION_DENY)
-
-        np_allow_name = f"tft-np-nssel-allow-{self.index}"
-        in_file_template = tftbase.get_manifest("network-policy-ns-selector.yaml.j2")
-        out_file_yaml = tftbase.get_manifest_renderpath(
-            f"np-nssel-{self.pod_name}.yaml"
+        return self.create_network_policy(
+            port, NP_ACTION_ALLOW, ns_name=allow_namespace
         )
 
-        template_args = {
-            **self.get_template_args(),
-            "np_allow_name": np_allow_name,
-            "np_ns_name": _j(allow_namespace),
-            "np_ns_port": _j(port),
-        }
-
-        self.render_file(
-            "Namespace Selector Network Policy",
-            in_file_template,
-            out_file_yaml,
-            template_args,
-        )
-        self.run_oc(
-            f"apply -f {out_file_yaml}",
-            check_success=lambda r: r.success or "already exists" in r.err,
-            die_on_error=True,
-        )
-        return self.run_oc(
-            f"get networkpolicies {np_allow_name}",
-            die_on_error=True,
-        ).out
-
-    def create_network_policy(self, port: int, action: str = NP_ACTION_DENY) -> str:
+    def create_network_policy(
+        self,
+        port: int,
+        action: str = NP_ACTION_DENY,
+        *,
+        ns_name: Optional[str] = None,
+    ) -> str:
         action = action.lower()
-        np_name = f"tft-np-{action}-{self.index}"
+        np_prefix = "tft-np-nssel" if ns_name else "tft-np"
+        np_name = f"{np_prefix}-{action}-{self.index}"
         in_file_template = tftbase.get_manifest("network-policy.yaml.j2")
         out_file_yaml = tftbase.get_manifest_renderpath(
             f"np-{self.pod_name}-{action}.yaml"
@@ -951,10 +932,11 @@ class Task(ABC):
             "np_name": np_name,
             "np_action": action,
             "np_port": _j(port),
+            "np_ns_name": _j(ns_name) if ns_name else False,
         }
 
         self.render_file(
-            f"{action.title()} Network Policy",
+            f"{'Namespace Selector ' if ns_name else ''}{action.title()} Network Policy",
             in_file_template,
             out_file_yaml,
             template_args,
